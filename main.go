324c6049a810fa0c76c180a605d92d2f030df7f6
attendee <attendees@atomicmail.io>
2025-12-18 07:51:01 +0300

    add: arc choose

--- main.go
+++ main.go
@@ -1,7 +1,94 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+type Story map[string]Arc
+
+type Arc struct {
+	Title   string   `json:"title"`
+	Story   []string `json:"story"`
+	Options []Option `json:"options"`
+}
+
+type Option struct {
+	Text string `json:"text"`
+	Arc  string `json:"arc"`
+}
+
+func printArc(a Arc) {
+	fmt.Printf("Title: %s\n", a.Title)
+
+	fmt.Printf("Story: \n")
+	for _, s := range a.Story {
+		fmt.Printf("\t%s\n\n", s)
+	}
+
+	fmt.Printf("Options: \n")
+	for i, o := range a.Options {
+		fmt.Printf("%d %s\n", i, o.Text)
+	}
+
+}
+
+func clearTerminal() {
+	var cmd *exec.Cmd
+
+	cmd = exec.Command("clear")
+
+	cmd.Stdout = os.Stdout
+
+	cmd.Run()
+}
 
 func main() {
-	fmt.Println("adventure")
+	path := flag.String("path", "", "Path to story")
+	flag.Parse()
+
+	file, err := os.ReadFile(*path)
+	if err != nil {
+		panic(err)
+	}
+
+	var story Story
+
+	json.Unmarshal(file, &story)
+
+	// fmt.Println("Contents of file", string(file))
+
+	defer fmt.Println("adventure")
+
+	a := story["intro"]
+	for {
+		clearTerminal()
+
+		printArc(a)
+		if len(a.Options) == 0 {
+			os.Exit(0)
+		}
+
+		fmt.Printf("Choose arc: ")
+
+		var c int
+
+		for {
+
+			fmt.Scanf("%d", &c)
+
+			if c < 0 || c >= len(a.Options) {
+				continue
+			} else {
+				break
+			}
+		}
+
+		a = story[a.Options[c].Arc]
+
+	}
+
 }
